f2a1c1a961a50613df97fa34efe85be266fd0789
Rudransh Singh Athwal <rsathwal04@gmail.com>
2025-09-02 01:31:44 +0530

    added codeforces link in contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -177,6 +177,26 @@ export default function Contact() {
             </svg>
           </button>
         </a>
+
+        <a
+          href="https://codeforces.com/profile/rsathwal04"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button
+            type="button"
+            class="text-white hover:text-[#1F8ACB] rounded-sm p-3 text-center inline-flex items-center mr-3 my-1.5"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M4.5 7.5C5.328 7.5 6 8.172 6 9v10.5c0 .828-.672 1.5-1.5 1.5h-3C.673 21 0 20.328 0 19.5V9c0-.828.673-1.5 1.5-1.5h3zm9-4.5c.828 0 1.5.672 1.5 1.5v15c0 .828-.672 1.5-1.5 1.5h-3c-.827 0-1.5-.672-1.5-1.5v-15c0-.828.673-1.5 1.5-1.5h3zm9 7.5c.828 0 1.5.672 1.5 1.5v7.5c0 .828-.672 1.5-1.5 1.5h-3c-.828 0-1.5-.672-1.5-1.5V12c0-.828.672-1.5 1.5-1.5h3z" />
+            </svg>
+          </button>
+        </a>
       </div>
     </section>
   );
